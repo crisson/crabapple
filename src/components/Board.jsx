4afe72638623089acfb0370c347d060b641d0c10
Crisson Jno-Charles <crissonjc@gmail.com>
2015-03-15 15:26:29 -0400

    fill board with dummy data

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,36 @@
+import lodash from 'lodash'
+
 import React from 'react';
 
 import {Paper} from 'material-ui'
+
+import Cell from './Cell.jsx'
+import EmptyCell from './EmptyCell.jsx'
+
+const pieces = lodash.fill(new Array(15), 1)
+    .map(() => lodash.fill(new Array(15), 1))
 
 export default React.createClass({
 
     displayName: 'Board',
 
+    makeCells(){
+        return pieces.map((row, y) => {
+            return row.map((value, x) => {
+                let key = `(${y},${x})`
+                return (<Cell key={key} x={x} y={y}/>)
+            })
+        })
+    },
+
     render() {
+
+        // let cells = []
+        let cells = this.makeCells()
+
         return (
             <Paper className="board" zDepth={1}>
-                board
+                {cells}
             </Paper>
         );
     }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,12 +1,34 @@
+import lodash from 'lodash'
+
 import React from 'react';
+
+import {Paper} from 'material-ui'
+
+const letters = lodash.toArray('abcdefghijklmnopqrstuvwxyz').map(_ => _.toUpperCase())
 
 export default React.createClass({
 
     displayName: 'Cell',
 
+    getDefaultProps() {
+        return {};
+    },
+
+    propTypes: {
+        x: React.PropTypes.number.isRequired,
+        y: React.PropTypes.number.isRequired,
+        value: React.PropTypes.string
+    },
+
     render() {
+        let value = letters[lodash.random(letters.length - 1)]
+
         return (
-            <div className="cell" />
+            <div className="cell">
+                <span className="cell-content">
+                    <Paper zDepth={1}> {value} </Paper>
+                </span>
+            </div>
         );
     }
 

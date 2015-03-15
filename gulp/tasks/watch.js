13b56eb47831f73424cf1ec2b043bdb656a6e270
Crisson Jno-Charles <crissonjc@gmail.com>
2015-03-15 10:29:55 -0400

    update watch task to include jade task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,10 +3,12 @@ var lessConfig = require('../config').less
 var lessAlienConfig = require('../config').lessAlien
 var config = require('../config').browserify
 var copyConfig = require('../config').copy
+var jadeConfig = require('../config').jade
 
 gulp.task('watch', ['build'], function(){
     gulp.watch(lessConfig.watch, ['less'])
     gulp.watch(lessAlienConfig.watch, ['less-alien'])
-	gulp.watch(copyConfig.watch, ['copy'])
+    gulp.watch(copyConfig.watch, ['copy'])
+	gulp.watch(jadeConfig.watch, ['jade'])
     return gulp.watch(config.watch, { interval: 500 }, ['browserify'])
 })

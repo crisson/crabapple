a645443046e13cf3696c046b19596201698067f0
Crisson Jno-Charles <crissonjc@gmail.com>
2015-05-09 19:36:28 -0400

    change default reflux promise library

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import reactTapEventPlugin from 'react-tap-event-plugin'
 
 import React from 'react'
+import Reflux from 'reflux'
+import Bluebird from 'bluebird'
 
 reactTapEventPlugin()
 
+Reflux.setPromise(Bluebird)
+
 import Homepage from '@crabapple/Homepage.jsx'
 
-React.render(<Homepage />, document.querySelector('#app'))
+React.render( < Homepage / > , document.querySelector('#app'))
